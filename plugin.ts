1b20ac3f7392e7cbe978759060507a1129f58568
Tim Reichen <tim.reichen@gmail.com>
2020-08-01 14:34:58 +0200

    fmt

--- plugin.ts
+++ plugin.ts
@@ -4,14 +4,12 @@ export type Transform = (path: string, source: string) => Promise<string>;
 
 export type Plugin = (path: string, source: string) => Promise<string>;
 
-export function plugin(
-  { name, include, exclude, transform }: {
-    name: string;
-    include?: Include;
-    exclude?: Exclude;
-    transform: Transform;
-  },
-): Plugin {
+export function plugin({ name, include, exclude, transform }: {
+  name: string;
+  include?: Include;
+  exclude?: Exclude;
+  transform: Transform;
+}): Plugin {
   return async (path: string, source: string) => {
     if (include && !include(path)) return source;
     if (exclude && exclude(path)) return source;

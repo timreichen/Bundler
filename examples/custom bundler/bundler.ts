fe69edf14a072684edafe71a6482c7376e097dc5
Tim Reichen <tim.reichen@gmail.com>
2020-08-31 15:22:32 +0200

    update examples

--- examples/custom bundler/bundler.ts
+++ examples/custom bundler/bundler.ts
@@ -1,12 +1,13 @@
 import { bundle } from "../../mod.ts";
 import { ensureFile } from "https://deno.land/std@0.66.0/fs/mod.ts";
+import { CompilerOptions } from "../../typescript.ts";
 
 const input = `src/index.ts`;
 const output = `dist/index.js`;
 const source = await Deno.readTextFile(input);
 const inputMap = { [input]: source };
 const outputMap = { [input]: output };
-const compilerOptions = { target: "ES5" } as any;
+const compilerOptions = { target: "ES5" } as CompilerOptions;
 const modules = await bundle(inputMap, outputMap, { compilerOptions });
 
 for (const [output, source] of Object.entries(modules)) {
--- examples/top level await/src/world.ts
+++ examples/top level await/src/world.ts
@@ -1,12 +1,1 @@
 export const world = "World";
-export default "test";
-export function hello() {
-}
-const a = "a";
-const b = "b";
-const c = "c";
-export {
-  a,
-  b,
-  c as d,
-};

eca693d713750c6a4a53c2f915cc1cf659b72952
Tim Reichen <tim.reichen@gmail.com>
2020-10-15 13:02:30 +0200

    update plugin system

--- plugins/loaders/css_test.ts
+++ plugins/loaders/css_test.ts
@@ -1,7 +1,13 @@
 import { cssLoader } from "./css.ts";
 import { assertEquals } from "https://deno.land/std@0.74.0/testing/asserts.ts";
 
-Deno.test("cssLoader imports", async () => {
+Deno.test("cssLoader test", async () => {
+  assertEquals(await cssLoader().test("testdata/src/styles.css"), true);
+  assertEquals(await cssLoader().test("testdata/src/a.ts"), false);
+});
+
+
+Deno.test("cssLoader fn", async () => {
   const input = "testdata/src/a.ts";
   const source = `
     @import "a.css";

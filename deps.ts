ca6a998d4904bc2c9f7619f8bf0fe94f846fdac1
timreichen <tim.reichen@gmail.com>
2021-01-02 05:09:19 +0100

    update deps

--- deps.ts
+++ deps.ts
@@ -11,11 +11,10 @@ export { Program } from "https://deno.land/x/program@0.1.6/mod.ts";
 export type { Args } from "https://deno.land/std@0.81.0/flags/mod.ts";
 export { invalidSubcommandError } from "https://deno.land/x/program@0.1.6/_helpers.ts";
 
-export { default as ts } from "https://esm.sh/typescript@4.0.5";
-export * as postcss from "https://esm.sh/postcss";
+export { default as ts } from "https://esm.sh/typescript@4.1.3";
+export * as postcss from "https://esm.sh/postcss@8.2.2";
 export { default as posthtml } from "https://esm.sh/posthtml@0.15.1";
 
 export { default as postcssPresetEnv } from "https://esm.sh/postcss-preset-env@6.7.0?no-check";
-export * as terser from "https://esm.sh/terser@5.3.8";
-export { default as csso } from "https://esm.sh/csso@4.0.3";
-export * as sass from "https://x.nest.land/sass@2.0.0/mod.ts";
+export * as terser from "https://esm.sh/terser@5.5.1";
+export { default as csso } from "https://esm.sh/csso@4.2.0";

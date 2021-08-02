c576e44815d3a458a73b3571f2fdfc0ef501ba1c
Andrew Morris <voltrevo@gmail.com>
2021-08-03 09:38:20 +1000

    Enable ts config with spa_server_cli.ts

--- spa_server_cli.ts
+++ spa_server_cli.ts
@@ -1,13 +1,21 @@
 #!/usr/bin/env -S deno run --unstable --allow-net --allow-read --allow-write
 
 import { parse } from "https://deno.land/std@0.100.0/flags/mod.ts";
+import { Bundler } from "./bundler.ts";
 import { Server } from "./server.ts";
+import { defaultPlugins } from "./_bundler_utils.ts";
 import { createOptions } from "./_util.ts";
 
 class SpaServer extends Server {
   index: string;
-  constructor({ index = "index.html" }: { index?: string } = {}) {
-    super();
+  constructor({
+    index = "index.html",
+    bundler = new Bundler(defaultPlugins()),
+  }: {
+    index?: string;
+    bundler?: Bundler;
+  } = {}) {
+    super({ bundler });
     this.index = index;
   }
   async handle(request: Request) {
@@ -25,12 +33,18 @@ const args = parse(Deno.args);
 const { port = 8000 } = args;
 const { inputs, ...options } = await createOptions(args);
 
+const bundler = new Bundler(
+  defaultPlugins({
+    typescriptCompilerOptions: options.compilerOptions,
+  }),
+);
+
 const input = inputs[0];
 const index = "index.html";
 options.outputMap = {
   ...options.outputMap,
   [input]: index,
 };
-const server = new SpaServer({ index });
+const server = new SpaServer({ index, bundler });
 await server.bundle(inputs, options);
 await server.listen({ port });
